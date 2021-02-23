dbe0f2bdd600394986263cd7c80c4e95d18460a4
Alireza Sadeghi <aliraza7926@gmail.com>
2021-02-23 11:24:34 +0330

    fix a typo

--- api/views.py
+++ api/views.py
@@ -6,4 +6,4 @@ from .serializers import BookSerializer
 
 class BookAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
-    serializers_class = BookSerializer
+    serializer_class = BookSerializer
